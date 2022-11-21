5dc9cc4b88ffc7a01a785d3464d66bbf563be30c
Salim DELLALI <dellali.salim@gmail.com>
2022-11-21 21:27:00 +0100

    (setup): added db connection, added .env.example for reference, edit minor typos

---
+++ src/config/database.config.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+export class MongoDBConfiguration {
+  static instance: MongoDBConfiguration;
+
+  private constructor() {}
+
+  static get(): MongoDBConfiguration {
+    if (!MongoDBConfiguration.instance) {
+      MongoDBConfiguration.instance = new MongoDBConfiguration();
+    }
+    return MongoDBConfiguration.instance;
+  }
+
+  async connect(connectionString: string) {
+    // @TODO insert a logger
+    try {
+      await mongoose.connect(connectionString);
+      console.log('Connection to MongoDB established');
+    } catch (error: any) {
+      console.error(`Error connecting to MongoDB : ${error.message}`);
+      process.exit(1);
+    }
+  }
+}
+
+const mongoDBConfiguration = MongoDBConfiguration.get();
+export { mongoDBConfiguration as Database };
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
+import { Database } from './config/database.config';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app: Express = express();
+const PORT = process.env.PORT || 2000;
 const API_VERSION = '/api/v1';
 
+Database.connect(process.env.MONGODB_URL!);
 app.use(cors());
 app.use(express.json());
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
-});
+// healthcheck route
+app.get('/ping', (req: Request, res: Response) => res.send('pong'));
 
 app.listen(PORT, () => {
   console.log('Web server running on port: ' + PORT);
